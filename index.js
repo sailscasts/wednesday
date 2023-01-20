fb13cb7190d3189e34792a3c26025648b847b37c
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-01-20 17:00:15 +0100

    fix: use legacy node modules

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs = require('node:fs')
-const path = require('node:path')
+const fs = require('fs')
+const path = require('path')
 require('dotenv').config()
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js')
 
